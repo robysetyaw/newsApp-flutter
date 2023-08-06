3bc2ac0364a9257354d5798c4c8e30b1f75553c2
robysetyaw <robysetyaw@gmail.com>
2023-08-06 14:38:11 +0700

    add CategoriesScreen Widget

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,50 @@ class NewsApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
+      home: CategoriesScreen(),
+    );
+  }
+}
+
+class CategoriesScreen extends StatelessWidget {
+  final List<String> categories = [
+    'general',
+    'business',
+    'technology',
+    'entertainment',
+    'health',
+    'science',
+    'sports',
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('News Categories'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () {
+              showSearch(context: context, delegate: SearchScreen());
+            },
+          ),
+        ],
+      ),
+      body: ListView.builder(
+        itemCount: categories.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(categories[index]),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => SourceScreen(category: categories[index])),
+              );
+            },
+          );
+        },
+      ),
     );
   }
 }
